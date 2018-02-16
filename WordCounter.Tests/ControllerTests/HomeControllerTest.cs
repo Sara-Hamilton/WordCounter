999ce2eac659058cb853fdcc8098810adaf64e05
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-16 11:12:27 -0800

    add controller test methods

--- WordCounter.Tests/ControllerTests/HomeControllerTest.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTest.cs
@@ -9,6 +9,27 @@ namespace WordCounter.Tests
   [TestClass]
   public class HomeControllerTest
   {
-    
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
+    {
+      //arrange
+      HomeController controller = new HomeController();
+      //act
+      IActionResult indexView = controller.Index();
+      ViewResult result = indexView as ViewResult;
+      //assert
+      Assert.IsInstanceOfType(result, typeof(ViewResult));
+    }
+
+    [TestMethod]
+    public void Index_HasCorrectModelType_RepeatCounter()
+    {
+      //arrange
+      ViewResult indexView = new HomeController().Index() as ViewResult;
+      //act
+      var result = indexView.ViewData.Model;
+      //assert
+      Assert.IsInstanceOfType(result, typeof(RepeatCounter));
+    }
   }
 }
