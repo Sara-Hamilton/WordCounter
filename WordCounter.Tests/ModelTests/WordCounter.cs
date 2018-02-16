1ca48987bf7f85521039ac261e5b7bbf9341cb1b
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-16 09:12:09 -0800

    add red consition for getter and setter test methods

--- WordCounter.Tests/ModelTests/WordCounter.cs
+++ WordCounter.Tests/ModelTests/WordCounter.cs
@@ -7,6 +7,18 @@ namespace WordCounter.Tests
 {
   [TestClass]
   public class WordCounterTest
-  
+  {
+    [TestMethod]
+    public void GetsAndSets_AllGettersAndSetters_String()
+    {
+        //arrange
+        RepeatCounter newRepeatCounter = new RepeatCounter("the", "the girl likes the food");
+        //action
+        string resultWord = newRepeatCounter.GetInputWord();
+        string resultString = newRepeatCounter.GetInputString();
+        //assert
+        Assert.AreEqual("they", resultWord);
+        Assert.AreEqual("girl likes the food", resultString);
+      }
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -6,6 +6,35 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    private string _inputWord;
+    private string _inputString;
+
+
+    public RepeatCounter(string inputWord, string inputString)
+    {
+      _inputWord = inputWord;
+      _inputString = inputString;
+    }
+
+    public string GetInputWord()
+    {
+      return _inputWord;
+    }
+
+    public void SetInputWord(string inputWord)
+    {
+      _inputWord = inputWord;
+    }
+
+    public string GetInputString()
+    {
+      return _inputString;
+    }
+
+    public void SetInputString(string inputString)
+    {
+      _inputString = inputString;
+    }
 
   }
 }
