80791fce2c139a9cba5f311963ad6b040e946ac3
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-16 08:48:37 -0800

    create skeleton with README

---
+++ WordCounter.Tests/ModelTests/WordCounter.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTest
+  
+  }
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WordCounter.Models
+{
+  public class RepeatCounter
+  {
+
+  }
+}
